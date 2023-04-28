597b5b7a2929017fc97c6b5cc546a613edb1b823
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-04-29 01:07:13 +0300

    Добавляет хедер

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+/* Menu */
+
+const header = document.querySelector('.main-header');
+const navToggle = document.querySelector('.main-header__toggle');
+
+header.classList.remove('main-header--no-js');
+
+navToggle.addEventListener('click', () => {
+  if (header.classList.contains('main-header--opened')) {
+    header.classList.remove('main-header--opened');
+  } else {
+    header.classList.add('main-header--opened');
+  }
+});
